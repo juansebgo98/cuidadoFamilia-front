72e152fcea01f2bbf8d3d48bf82347100de1b4e4
juansebgo98 <juan-sebastian2009@hotmail.es>
2024-05-19 12:59:17 +0200

    Creado componente Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { PantallaPrincipalComponent } from './Pages/pantalla-principal/pantalla-principal.component';
+import { HeaderComponent } from './Components/header/header.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/pantalla-principal', pathMatch: 'full' },
+  { path: 'pantalla-principal', component: PantallaPrincipalComponent },
+  // Agrega más rutas aquí según sea necesario
+];
 
 @NgModule({
   declarations: [
-    AppComponent,  ],
+    AppComponent,
+    PantallaPrincipalComponent,
+    HeaderComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
