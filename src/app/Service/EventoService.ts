c3fb9b0296e10986309da3748d8267a30b381560
juansebgo98 <juan-sebastian2009@hotmail.es>
2024-05-19 15:41:54 +0200

    Añadidos mas elementos de pantalla

--- src/app/Service/EventoService.ts
+++ src/app/Service/EventoService.ts
@@ -7,15 +7,15 @@ import { Evento } from '../Entity/Evento';
   providedIn: 'root'
 })
 export class EventoService {
-  constructor() {}
+  constructor() { }
 
   cargarEventos(): Observable<Evento[]> {
     const eventos: Evento[] = [
-      new Evento(1, startOfDay(new Date()), addDays(startOfDay(new Date()), 1), 'Consulta Médica', 'assets/icons/cirujano.png','Cita'),
-      new Evento(2, addDays(startOfDay(new Date()), -1), addDays(startOfDay(new Date()), 2), 'Medicina 1', 'assets/icons/medicina.png','Mecidamento'),
-      new Evento(3, addDays(startOfDay(new Date()), 3), addDays(startOfDay(new Date()), 4), 'Medicina 2', 'assets/icons/medicina.png','Mecidamento'),
-      new Evento(4, addDays(startOfDay(new Date()), -5), addDays(startOfDay(new Date()), -3), 'Consulta General', 'assets/icons/cirujano.png','Cita'),
-      new Evento(5, addDays(startOfDay(new Date()), 7), addDays(startOfDay(new Date()), 7), 'Revisión Anual', 'assets/icons/medicina.png','Cita')
+      new Evento(1, startOfDay(new Date()), addDays(startOfDay(new Date()), 1), 'Consulta Médica', 'assets/icons/cirujano.png', 'Cita'),
+      new Evento(2, addDays(startOfDay(new Date()), -1), addDays(startOfDay(new Date()), 2), 'Medicina 1', 'assets/icons/medicina.png', 'Mecidamento'),
+      new Evento(3, addDays(startOfDay(new Date()), 3), addDays(startOfDay(new Date()), 4), 'Medicina 2', 'assets/icons/medicina.png', 'Mecidamento'),
+      new Evento(4, addDays(startOfDay(new Date()), -5), addDays(startOfDay(new Date()), -3), 'Consulta General', 'assets/icons/cirujano.png', 'Cita'),
+      new Evento(5, addDays(startOfDay(new Date()), 7), addDays(startOfDay(new Date()), 7), 'Revisión Anual', 'assets/icons/medicina.png', 'Cita')
     ];
     return of(eventos);
   }
